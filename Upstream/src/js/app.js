b8182454a31dbd66bde4930b56e2ba4b7f64cb1b
Stephanie Fay <stephaniemfay@gmail.com>
2019-12-06 01:19:27 -0300

    solving persistence problem

--- Upstream/src/js/app.js
+++ Upstream/src/js/app.js
@@ -7,13 +7,11 @@ App = {
     return App.initWeb3();
   },
 
-  initWeb3: function() {
+  initWeb3: async function() {
     if (typeof web3 !== 'undefined') {
-      // If a web3 instance is already provided by Meta Mask.
       App.web3Provider = web3.currentProvider;
       web3 = new Web3(web3.currentProvider);
     } else {
-      // Specify default instance if no web3 instance provided
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
       web3 = new Web3(App.web3Provider);
     }
@@ -22,11 +20,8 @@ App = {
 
   initContract: function() {
     $.getJSON("Upstream.json", function(upstream) {
-      // Instantiate a new truffle contract from the artifact
       App.contracts.Upstream = TruffleContract(upstream);
-      // Connect provider to interact with contract
       App.contracts.Upstream.setProvider(App.web3Provider);
-
       return App.render();
     });
   },
@@ -38,15 +33,14 @@ App = {
     loader.show();
     content.hide();
 
-    App.showAmount();
-  
-    // Load account data
-    web3.eth.getCoinbase(function(err, account) {
+  web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
         $("#accountAddress").html("Endereço: " + account);
       }
     });
+
+    App.showAmount();
   },
 
   showAmount: function() {
@@ -59,20 +53,16 @@ App = {
 
   addAmount: function() {
     const investment = $('#investment').val();
-
-    var upstreamInstance;
-
+    console.log(investment);
     App.contracts.Upstream.deployed().then(function(instance) {
-      upstreamInstance = instance;
-      const investmentInt = parseInt(investment);
-      return upstreamInstance.addAmount(investmentInt, { from: App.account, gas:30000000 });
+      return instance.addAmount(investment, {from: App.account});
     }).then(function () {
       console.log('saved');
       window.location.reload();
     }).catch(function(err) {
       console.error(err);
     });
-  },
+  }
 };
 
 $(function() {
