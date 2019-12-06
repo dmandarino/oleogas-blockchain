afa58820b777f8c48f4d84e49a7ce8d9848766fa
Stephanie Fay <stephaniemfay@gmail.com>
2019-12-06 02:56:07 -0300

    app_index

---
+++ Upstream/src/js/app_index.js
@@ -0,0 +1,77 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON("Upstream.json", function(upstream) {
+      App.contracts.Upstream = TruffleContract(upstream);
+      App.contracts.Upstream.setProvider(App.web3Provider);
+
+      return App.render();
+    });
+  },
+
+  render: function() {  
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        App.showAmount();
+      }
+    });
+
+    //Load contract data
+  },
+
+  showAmount: function() {
+   console.log("opa");
+    App.contract.Upstream.deployed().then(function (instance) {
+      return instance.totalAmount();
+    }).then(function (total) {
+      console.log(total);
+      $("#totalInvestment").html(total);
+    });
+  },
+
+  addAmount: function() {
+    const investment = $('#investment').val();
+
+    var upstreamInstance;
+
+    App.contracts.Upstream.deployed().then(function(instance) {
+      upstreamInstance = instance;
+      const investmentInt = parseInt(investment);
+      return upstreamInstance.addAmount(investmentInt, { from: App.account });
+    }).then(function () {
+      console.log('saved');
+      window.location.reload();
+    }).catch(function(err) {
+      console.error(err);
+    });
+  },
+};
+
+$(function() {
+  $(window).load(function() {
+    console.log("oaoaoa");
+    App.init();
+  });
+});
